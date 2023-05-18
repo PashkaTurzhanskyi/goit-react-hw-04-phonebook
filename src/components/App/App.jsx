0f738c7f82931c00cceff79e61b2a55a84f21878
PashkaTurzhanskyi <pavlo.turzhanskiy@gmail.com>
2023-05-18 18:07:38 +0300

    upd App & Component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { ContactForm } from '../ContactForm/ContactForm';
 import { ContactList } from '../ContactList/ContactList';
